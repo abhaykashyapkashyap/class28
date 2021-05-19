fc7142010405fcdaad70be7b0ed13d4bc72aa401
abhay kashyap <83275855+abhaykashyapkashyap@users.noreply.github.com>
2021-05-19 15:25:23 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,24 @@
+class Chain {
+    constructor(bodyA,pointB){
+        var chainoptions={
+            bodyA:bodyA,
+            pointB:pointB,
+            length:10,
+            stiffness:0.5,
+        }
+        this.pointB=pointB
+        this.chain = Constraint.create(chainoptions)
+        World.add(world,this.chain)    
+    }
+    fly(){
+        this.chain.bodyA=null
+    }
+    display(){
+        if(this.chain.bodyA){
+        var pointA=this.chain.bodyA.position;
+        var pointB=this.pointB;
+        strokeWeight(5);
+        line(pointA.x,pointA.y,pointB.x,pointB.y)
+    }
+}
+}
